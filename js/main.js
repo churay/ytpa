efe7df037160a0ed924fef84c2e13a23d8219855
Joseph Ciurej <churayy@gmail.com>
2015-08-06 21:45:32 -0700

    Added the main Javascript file and added libraries to the index page.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * @file main.js
+ *
+ * The main script point for the "ytpa" application, which contains all the
+ * bootstrapping logic for the application.
+ */
+
+/// Entry Point ///
+
+function main() {
+    // TODO(JRC): Implement this function so that the page document is filled
+    // with the names of the 10 most recent uploads from some YouTube channel.
+    console.log( "Hello, world!" );
+}
+
+
+$( document ).ready( main );
