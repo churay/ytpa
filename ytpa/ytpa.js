71c9e2e13dd3ecd3379d07fc2b76edd1d7740bdc
Joseph Ciurej <churayy@gmail.com>
2015-08-26 22:12:07 -0700

    Implemented the initial (rough) version of the page layout for the tool.

--- ytpa/ytpa.js
+++ ytpa/ytpa.js
@@ -13,6 +13,8 @@
      * must be called before calling any other library functions.
      */
     ytpa.init = function() {
+        $("#ytpa-graph-container").height($("#ytpa-graph-container").width());
+
         gapi.client.setApiKey(ytpa.config.appid);
 
         gapi.client.load('youtube', 'v3').then(function() {
