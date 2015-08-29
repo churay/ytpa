c797c10c7cf595c151bbb0edb5e4ba8cb11d368d
timchow <timssss@live.com>
2015-08-28 23:14:13 -0500

    Implemented querying for arbitrary number of items for Uploads
    The updated ytpa.query.uploads function now returns an arbitrary number
    of items. Since this task required multiple calls to the YouTube api, I
    made use of the Promise.all function to resolve multiple promises. For
    more information:
    http://www.sitepoint.com/deeper-dive-javascript-promises/

--- ytpa/query.js
+++ ytpa/query.js
@@ -58,7 +58,9 @@
      * Returns a promise that returns all of the upload objects for a given user.
      */
     ytpa.query.uploads = function(user, numResults) {
-        var numResults = (numResults !== undefined) ? numResults : 10;
+        var numResults = (numResults !== undefined) ? numResults : 51;
+        var uploadsPLID;
+        var results = []
         var uplRequestOptions = {
             part: 'contentDetails',
             forUsername: user,
@@ -66,41 +68,81 @@
 
         var uplRequest = gapi.client.youtube.channels.list(uplRequestOptions);
         return uplRequest.then(function(response) {
-            var uploadsPLID = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+            uploadsPLID = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+
             var uplplRequestOptions = {
                 part: 'contentDetails',
                 playlistId: uploadsPLID,
-                maxResults: numResults,
+                maxResults: 50,
             };
 
+            if (numResults < 51)
+                uplplRequestOptions.maxResults = numResults;
+
+
             var uplplRequest = gapi.client.youtube.playlistItems.list(uplplRequestOptions);
-            return uplplRequest;
+            return uplplRequest; // returns a promise for the first numResults items
 
         }).then(function(response) {
-            var playlistBatchRequest = gapi.client.newBatch();
+            // this "then" clause will handle the logic for numResults > 50
+            var count = response.result.items.length;
+            var promises = [];
 
-            var playlistItems = response.result.items;
-            for(var videoIdx in playlistItems) {
-                var videoID = playlistItems[videoIdx].contentDetails.videoId;
-                var videoOptions = { part: 'snippet', id: videoID };
-
-                var videoRequest = gapi.client.youtube.videos.list(videoOptions);
-                playlistBatchRequest.add(videoRequest);
+            if (count == numResults) { // add response to array then return it - for numResults less than 51
+                promises.push(response);
+                return promises;
             }
 
-            return playlistBatchRequest;
+            var remaining = numResults - count;
+
+            while (response.result.nextPageToken && remaining != 0) {
+                var uplplRequestOptions = {
+                part: 'contentDetails',
+                playlistId: uploadsPLID,
+                nextPageToken : response.result.nextPageToken
+                };
+
+                if (remaining < 50) {
+                    uplplRequestOptions.maxResults = remaining;
+                    remaining = 0;
+                }
+                else {
+                    uplplRequestOptions.maxResults = 50;
+                    remaining -= 50;
+                }
+                var request = gapi.client.youtube.playlistItems.list(uplplRequestOptions);
+                promises.push(request);
+            }
+
+            return promises.concat(response); // need to combine the first numResults itmes with the rest
+        }).then(function(promises) {
+
+            return Promise.all(promises).then(function(promiseArr) {
+                var playlistBatchRequest = [];
+
+                promiseArr.forEach(function(response) { 
+                var playlistItems = response.result.items;
+
+                for(var videoIdx in playlistItems) {
+                    var videoID = playlistItems[videoIdx].contentDetails.videoId;
+                    var videoOptions = { part: 'snippet', id: videoID };
+
+                    var videoRequest = gapi.client.youtube.videos.list(videoOptions);
+                    playlistBatchRequest.push(videoRequest);
+                }
+            });
+                return playlistBatchRequest;
 
         }).then(function(response) {
-            var playlistResponseMap = response.result;
+                return Promise.all(response).then(function(promiseArr) {
+                    var playlistItems = []
 
-            var playlistItems = [];
-            for(var playlistResponseID in playlistResponseMap) {
-                var videoResponse = playlistResponseMap[playlistResponseID];
-                playlistItems.push(videoResponse.result.items[0].snippet);
-            }
-
-            return playlistItems;
-
+                    promiseArr.forEach(function(response) {
+                        playlistItems.push(response.result.items[0].snippet);
+                    });
+                return playlistItems;
+                });
+            });
         });
     };
 
