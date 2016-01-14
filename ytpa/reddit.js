dd4646d30640a663b056769cfe8720343085948d
timchow <timssss@live.com>
2016-01-13 23:06:03 -0600

    Modifying file name for redditjs.js to reddit.js

--- ytpa/reddit.js
+++ ytpa/reddit.js
@@ -1,16 +1,16 @@
 /**
- * @file redditjs
+ * @file reddit
  *
  * Temp Description
  */
 
 ( function(ytpa, $, undefined) {
     /// Public Members ///
-    ytpa.redditjs = ytpa.redditjs || {};
+    ytpa.reddit = ytpa.reddit || {};
 
     var root_url = "https://www.reddit.com";
 
-    ytpa.redditjs.search = function(query) {
+    ytpa.reddit.search = function(query) {
         reddit.search(query).t('all').limit(1).sort("top").fetch(function(res) {
             console.log(root_url + res.data.children[0].data.permalink);
         });
