dc2753526c58b397bbaafce4096f2586730d653c
Joseph Ciurej <churayy@gmail.com>
2016-02-03 20:55:05 -0800

    Converted all tabs to spaces in the 'reddit.js' file.

--- ytpa/reddit.js
+++ ytpa/reddit.js
@@ -12,31 +12,30 @@
     var youtube_url = "https://www.youtube.com"
 
     ytpa.reddit.getTopCommentForLink = function(videoId, channel) {
-    	var video_url = youtube_url + "/watch?v=" + videoId;
+        var video_url = youtube_url + "/watch?v=" + videoId;
 
         return reddit.search(video_url).t('all').limit(10).sort("top").fetch(function(res) {
-        	for (var idx in res.data.children) {
-        		var subreddit = res.data.children[idx].data.subreddit;
-        		
-        		if (subreddit.toLowerCase() == channel.toLowerCase()) {
-        			var id = res.data.children[idx].data.id;
-        			return ytpa.reddit.topComment(id, subreddit);
-        		}
-        	}
+            for (var idx in res.data.children) {
+                var subreddit = res.data.children[idx].data.subreddit;
+
+                if (subreddit.toLowerCase() == channel.toLowerCase()) {
+                    var id = res.data.children[idx].data.id;
+                    return ytpa.reddit.topComment(id, subreddit);
+                }
+            }
             return 5;
         });
 
     };
 
-	ytpa.reddit.topComment = function(id, subreddit) {
-    	return reddit.comments(id, subreddit).limit(1).sort("top").fetch(function(res) {
-		    return reddit_url + res[0].data.children[0].data.permalink;
-		});
+    ytpa.reddit.topComment = function(id, subreddit) {
+        return reddit.comments(id, subreddit).limit(1).sort("top").fetch(function(res) {
+            return reddit_url + res[0].data.children[0].data.permalink;
+        });
     }
 
-
     ytpa.reddit.test = function() {
-    	return reddit.new('programming').before("t3_23jf8n");
+        return reddit.new('programming').before("t3_23jf8n");
     }
  
 }(window.ytpa = window.ytpa || {}, jQuery) );
