f58b00cf23f018b99dfd9e0ef1c2dea16c6bbd4c
Joseph Ciurej <churayy@gmail.com>
2016-02-04 22:36:26 -0800

    Refactored and improved the "ytpa.reddit.topcomment" function.

--- ytpa/reddit.js
+++ ytpa/reddit.js
@@ -22,27 +22,21 @@
 
         return reddit.search(videoURL).t('all').limit(10).sort('top').fetch(
         ).then(function(response) {
-            for(var idx in response.data.children) {
-                var subreddit = response.data.children[idx].data.subreddit;
+            var bestThread = response.data.children[0];
+            for(var threadIdx in response.data.children) {
+                var thread = response.data.children[threadIdx].data;
 
-                if(subreddit.toLowerCase() == channel.toLowerCase())
-                    return {
-                        comment: response.data.children[idx].data.id,
-                        subreddit: response.data.children[idx].data.subreddit,
-                    };
+                if(thread.subreddit.toLowerCase() == channel.toLowerCase()) {
+                    bestThread = thread;
+                    break;
+                }
             }
 
-            return { comment: -1, subreddit: '' };
+            return reddit.comments(bestThread.id,
+                bestThread.subreddit).limit(1).sort('top').fetch();
 
         }).then(function(response) {
-            return reddit.comments(response.comment, response.subreddit).limit(1).sort('top').fetch();
-        });
-    };
-
-    // TODO(JRC): Remove this function once testing for the Reddit API is finished.
-    ytpa.reddit.test = function() {
-        reddit.new('programming').limit(10).fetch().then(function(response) {
-            console.log(response);
+            return response[1].data.children[0].data.body;
         });
     };
 
