532aede52e3ff2563cc93d9ebdae7ec22b8557a1
Tim Chow <timssss@live.com>
2016-03-19 00:15:03 -0500

    Styled the reddit frame to be contained inside the tooltip with proper wrappings

--- ytpa/templates.js
+++ ytpa/templates.js
@@ -12,7 +12,7 @@
 
     /** The string template for formal reddit comment displays. */
     ytpa.templates.redditcomment = `
-        <div class="well well-sm" style="width:{8}px">
+        <div class="well well-sm ytpa-reddit-frame" style="width:{8}px">
             <p class="text-left"><strong>{0}</strong> <small>[{1} points]</small></p>
             <p class="test-left md"><em>{3}</em></p>
             <p class="text-right">From <a href="{4}">{5}</a> on <a href="{6}">{7}</a></p>
