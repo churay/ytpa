53659b0d01f9c4d6e8a4fdcc9a2fe1471e01cf6f
Joseph Ciurej <churayy@gmail.com>
2019-08-30 20:36:51 -0700

    * Fixed the issue w/ the 'Spinner' component by sourcing from a local copy of a working version.

--- ytpa/ytpa.js
+++ ytpa/ytpa.js
@@ -75,16 +75,16 @@
             $('#ytpa-channel').keypress(function(e) {
                 if(e.which == 13) {
                     var target = $('.spinner-container').get(0);
-                    // var spinner = new Spinner(ytpa.config.spinner).spin(target);
-                    // $(document).on('populated', function() { spinner.stop(); });
+                    var spinner = new Spinner(ytpa.config.spinner).spin(target);
+                    $(document).on('populated', function() { spinner.stop(); });
                     queryNewChannel();
                 } 
             });
 
             $('#ytpa-channel-submit').click(function() {
                 var target = $('.spinner-container').get(0);
-                // var spinner = new Spinner(ytpa.config.spinner).spin(target);
-                // $(document).on('populated', function() { spinner.stop(); });
+                var spinner = new Spinner(ytpa.config.spinner).spin(target);
+                $(document).on('populated', function() { spinner.stop(); });
                 queryNewChannel();
             });
 
